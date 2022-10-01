15a6b37bdb04583b4bbd9c8744e5f8440d4dd739
Shayan Jamil <shayanjamil500@gmail.com>
2022-10-01 14:39:34 +0500

    Final commit of Nextjs NFT-Marketplace-Moralis

---
+++ components/NFTBox.jsx
@@ -0,0 +1,154 @@
+import React, { useEffect, useState } from "react";
+import { useWeb3Contract, useMoralis } from "react-moralis";
+import Image from "next/image";
+import { Card, useNotification } from "web3uikit";
+import { ethers } from "ethers";
+import nftMarketplaceAbi from "../constants/NftMarketplace.json";
+import nftAbi from "../constants/BasicNft.json";
+import UpdateListingModal from "./UpdateListingModal";
+
+//   ----------------
+
+const truncateStr = (fullStr, strLen) => {
+  if (fullStr.length <= strLen) return fullStr;
+
+  const separator = "...";
+  const seperatorLength = separator.length;
+  const charsToShow = strLen - seperatorLength;
+  const frontChars = Math.ceil(charsToShow / 2);
+  const backChars = Math.floor(charsToShow / 2);
+  return (
+    fullStr.substring(0, frontChars) +
+    separator +
+    fullStr.substring(fullStr.length - backChars)
+  );
+};
+
+//   ----------------
+const NFTBox = ({ price, nftAddress, tokenId, marketplaceAddress, seller }) => {
+  //   ----------------
+  const { isWeb3Enabled, account } = useMoralis();
+  const [imageURI, setImageURI] = useState("");
+  const [tokenName, setTokenName] = useState("");
+  const [tokenDescription, setTokenDescription] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const hideModal = () => setShowModal(false);
+  const dispatch = useNotification();
+
+  const { runContractFunction: getTokenURI } = useWeb3Contract({
+    abi: nftAbi,
+    contractAddress: nftAddress,
+    functionName: "tokenURI",
+    params: {
+      tokenId: tokenId,
+    },
+  });
+
+  const { runContractFunction: buyItem } = useWeb3Contract({
+    abi: nftMarketplaceAbi,
+    contractAddress: marketplaceAddress,
+    functionName: "buyItem",
+    msgValue: price,
+    params: {
+      nftAddress: nftAddress,
+      tokenId: tokenId,
+    },
+  });
+
+  async function updateUI() {
+    // get the token URI
+    // using the image tag from the token uri get the image
+    // let tokenURI = await getTokenURI();
+    const tokenURI =
+      "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    console.log(`The TokenURI is ${tokenURI}`);
+    if (tokenURI) {
+      // IPFS Gateway: A server that will return IPFS files from a "normal" URL.
+      const requestURL = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/");
+      const tokenURIResponse = await (await fetch(requestURL)).json();
+      const imageURI = tokenURIResponse.image;
+      const imageURIURL = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/");
+      setImageURI(imageURIURL);
+      setTokenName(tokenURIResponse.name);
+      setTokenDescription(tokenURIResponse.description);
+    }
+  }
+  //   ----------------
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      updateUI();
+    }
+  }, [isWeb3Enabled]);
+
+  const isOwnedByUser = seller === account || seller === undefined;
+  const formattedSellerAddress = isOwnedByUser
+    ? "you"
+    : truncateStr(seller || "", 15);
+
+  const handleCardClick = () => {
+    isOwnedByUser
+      ? setShowModal(true)
+      : buyItem({
+          onError: (error) => console.log(error),
+          onSuccess: handleBuyItemSuccess,
+        });
+  };
+
+  const handleBuyItemSuccess = async (tx) => {
+    await tx.wait(1);
+    dispatch({
+      type: "success",
+      message: "Item bought!",
+      title: "Item Bought",
+      position: "topR",
+    });
+  };
+
+  //   ----------------
+  return (
+    <div>
+      <div className="mx-2 my-2">
+        {imageURI ? (
+          <div>
+            <UpdateListingModal
+              isVisible={showModal}
+              tokenId={tokenId}
+              marketplaceAddress={marketplaceAddress}
+              nftAddress={nftAddress}
+              onClose={hideModal}
+            />
+
+            <Card
+              title={tokenName}
+              description={tokenDescription}
+              onClick={handleCardClick}
+            >
+              <div className="p-2">
+                <div className="flex flex-col items-end gap-2">
+                  <div>#{tokenId}</div>
+                  <div className="italic text-sm">
+                    Owned by {formattedSellerAddress}
+                  </div>
+                  <Image
+                    loader={() => imageURI}
+                    src={imageURI}
+                    height="200"
+                    width="200"
+                  />
+                  <div className="font-bold">
+                    {ethers.utils.formatUnits(price, "ether")} ETH
+                  </div>
+                </div>
+              </div>
+            </Card>
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NFTBox;
